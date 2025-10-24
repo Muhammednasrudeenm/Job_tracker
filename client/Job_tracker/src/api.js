2237e1b6cd95f2337e7443752c8ab6778c4e6ba7
Muhammednasrudeenm <101858466+Muhammednasrudeenm@users.noreply.github.com>
2025-10-24 22:40:57 +0530

    Fix formatting in api.js

--- client/Job_tracker/src/api.js
+++ client/Job_tracker/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://job-tracker-1-ybir.onrender.com'
+  baseURL: 'https://job-tracker-1-ybir.onrender.com',
 });
 
 export default api;
